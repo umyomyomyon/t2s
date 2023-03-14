b4c9975840975aa1a4a1d188798aa401edd87d5c
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-03-14 16:43:55 +0900

    ASTを取得

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Wolcome to t2s.");
+mod parse;
+mod cli;
+
+use cli::Cli;
+use clap::Parser;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+
+    let module = parse::get_ast_from_file(&args.path).unwrap();
+    parse::get_text_from_module(&module);
+    Ok(())
 }

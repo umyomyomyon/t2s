3a7b013597c678ac4988c34b1c6a125161727aa7
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-03-15 15:47:35 +0900

    describeブロックのspanを取得

--- assets/testcode.ts
+++ assets/testcode.ts
@@ -32,4 +32,63 @@ describe('hoge function tests', () => {
 function hoge() {
   console.log('hoge')
 }
+describe('hoge function tests', () => {
+  test('test1', () => {
+    expect(func(1, 2)).toBe(3)
+  })
+
+  test('test2', () => {
+    expect(func(82873, 987234)).toBe(1070107)
+  })
+
+  test('test3', () => {
+    expect(func(0, 1)).toThrowError(ZeroError)
+  })
+
+  test('test4', () => {
+    expect(func(1, 0)).toThrowError(ZeroError)
+  })
+
+  test('test5', () => {
+    expect(func(0, 0)).toThrowError(ZeroError)
+  })
+
+  test('test6', () => {
+    expect(func(-1, 1)).toBe(0)
+  })
+
+  test('test7', () => {
+    expect(func(1, -1)).toBe(0)
+  })
+})
+
+describe('hoge function tests', () => {
+  test('test1', () => {
+    expect(func(1, 2)).toBe(3)
+  })
+
+  test('test2', () => {
+    expect(func(82873, 987234)).toBe(1070107)
+  })
+
+  test('test3', () => {
+    expect(func(0, 1)).toThrowError(ZeroError)
+  })
+
+  test('test4', () => {
+    expect(func(1, 0)).toThrowError(ZeroError)
+  })
+
+  test('test5', () => {
+    expect(func(0, 0)).toThrowError(ZeroError)
+  })
+
+  test('test6', () => {
+    expect(func(-1, 1)).toBe(0)
+  })
+
+  test('test7', () => {
+    expect(func(1, -1)).toBe(0)
+  })
+})
   
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
 
     let module = parse::get_ast_from_file(&args.path).unwrap();
-    parse::get_text_from_module(&module);
+    let spans = parse::get_describe_spans(&module);
+    for span in spans {
+        println!("describe: {} | {}", span.lo.0, span.hi.0);
+    }
     Ok(())
 }

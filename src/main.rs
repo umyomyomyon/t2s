008b7a1fd2ae4998750089803d337a64347aad48
umyomyomyon <minecraft.sentry.yym@gmail.com>
2023-03-15 22:52:40 +0900

    multiple request

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let imports = parse::get_import_spans(&module);
     let import_text = parse::make_import_decl_text(&imports, &args.path).unwrap();
     let spans = parse::get_describe_spans(&module);
-    let code = import_text + parse::get_text_from_span(&spans[0], &args.path).unwrap().as_str();
-    println!("{}", code);
-    let response = api::request_chatgpt(&code).await.unwrap();
-    for choise in response.choices {
-        output::make_file(&choise.message.content).unwrap();
+    for (i, span) in spans.iter().enumerate() {
+        let code = import_text.clone() + parse::get_text_from_span(&span, &args.path).unwrap().as_str();
+        let response = api::request_chatgpt(&code).await.unwrap();
+        for choise in response.choices {
+            output::make_file(&choise.message.content, i.to_string()).unwrap();
+        }
     }
 
     Ok(())
--- src/output.rs
+++ src/output.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
 use std::io::Write;
 
-pub fn make_file(code: &String) -> Result<(), Box<dyn std::error::Error>> {
-    let mut file = File::create("output.js")?;
+pub fn make_file(code: &String, filename: String) -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::create(filename + ".js")?;
     file.write_all(code.as_bytes())?;
     Ok(())
 }
